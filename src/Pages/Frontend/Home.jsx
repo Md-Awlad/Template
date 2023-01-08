59c3a173b5ae1145bcb6ff29c57b3b5a268c3438
naser-rifat <rifatmahmud971@gmail.com>
2023-01-08 18:17:51 +0600

    add

--- src/Pages/Frontend/Home.jsx
+++ src/Pages/Frontend/Home.jsx
@@ -12,13 +12,16 @@ const Home = () => {
       ) : (
         <div className="h-screen flex justify-center items-center ">
           <Box className="text-center">
-            <Box>
+            <Box className="flex justify-center">
               <img
+                className="w-32 h-32 object-cover p-5"
                 src="https://i.ibb.co/0q5B8VP/MainLogo.png"
                 alt="nexis logo"
               />
             </Box>
-            <Typography component="div">No Subscription Available</Typography>
+            <Typography component="div" className="text-red-500">
+              No Subscription Available
+            </Typography>
             {/* <LocalDiningIcon sx={{ fontSize: 80 }} />
             <Typography component="span">Digital Menu</Typography> */}
           </Box>
